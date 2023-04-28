0291cc645b29b234bcc57ebb779956c7cc909741
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-04-28 22:01:27 +0000

    Fixed logic `after_request` to only happen for POST.

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask, request
 from config import get_config
 from .extensions import db, logs, migrate
 
-
 def create_app(test=False):
     config = get_config(test=test)
     app = Flask(__name__)
@@ -19,11 +18,13 @@ def create_app(test=False):
 
     @app.after_request
     def after_request(response):
+        if request.method != 'POST':
+            return response
         """ Logging after every POST request only if it isn't an introspection query. """
         json_data = request.get_json()
         is_introspection_query = bool(json_data and json_data.get(
             'operationName', False) == 'IntrospectionQuery')
-        if request.method == 'POST' and not is_introspection_query:
+        if not is_introspection_query:
             logger = logging.getLogger('api.access')
             logger.info(
                 '%s [%s] %s %s %s %s %s %s %s',
